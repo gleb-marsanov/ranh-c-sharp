ada586369ffe92eb3ebcd82548884b985a6233f8
Gleb Marsanov <gleb.marsanov@gmail.com>
2023-09-12 20:40:01 +0400

    Create string splitter

---
+++ hw7/hw7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace hw7;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("Введите предложение: ");
+        string sentence = Console.ReadLine()!;
+
+        var words = SplitString(sentence);
+        WriteEnumerableToConsole(words);
+    }
+
+    private static void WriteEnumerableToConsole(MatchCollection words) =>
+        Console.WriteLine(string.Join('\n', words));
+
+    private static MatchCollection SplitString(string sentence) => 
+        Regex.Matches(sentence, @"\b\w+\b", RegexOptions.IgnoreCase);
+}
